feb6438a18dd1c25ba16cb41c5426c2d171e9f48
Dennis Eichhorn <spl1nes.com@googlemail.com>
2015-12-30 22:51:05 +0100

    New admin classes (activate, deactivate, uninstall, update)

---
+++ Admin/Deactivate.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.0
+ *
+ * @category   TBD
+ * @package    TBD
+ * @author     OMS Development Team <dev@oms.com>
+ * @author     Dennis Eichhorn <d.eichhorn@oms.com>
+ * @copyright  2013 Dennis Eichhorn
+ * @license    OMS License 1.0
+ * @version    1.0.0
+ * @link       http://orange-management.com
+ */
+namespace Modules\HumanResourceManagement\Admin;
+
+use phpOMS\DataStorage\Database\DatabaseType;
+use phpOMS\DataStorage\Database\Pool;
+use phpOMS\Module\DeactivateAbstract;
+
+/**
+ * Navigation class.
+ *
+ * @category   Modules
+ * @package    Modules\Admin
+ * @author     OMS Development Team <dev@oms.com>
+ * @author     Dennis Eichhorn <d.eichhorn@oms.com>
+ * @license    OMS License 1.0
+ * @link       http://orange-management.com
+ * @since      1.0.0
+ */
+class Deactivate extends DeactivateAbstract
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function deactivate(Pool $dbPool, array $info)
+    {
+        parent::deactivate($dbPool, $info);
+    }
+}
---
+++ Admin/Uninstall.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.0
+ *
+ * @category   TBD
+ * @package    TBD
+ * @author     OMS Development Team <dev@oms.com>
+ * @author     Dennis Eichhorn <d.eichhorn@oms.com>
+ * @copyright  2013 Dennis Eichhorn
+ * @license    OMS License 1.0
+ * @version    1.0.0
+ * @link       http://orange-management.com
+ */
+namespace Modules\HumanResourceManagement\Admin;
+
+use phpOMS\DataStorage\Database\DatabaseType;
+use phpOMS\DataStorage\Database\Pool;
+use phpOMS\Module\UninstallAbstract;
+
+/**
+ * Navigation class.
+ *
+ * @category   Modules
+ * @package    Modules\Admin
+ * @author     OMS Development Team <dev@oms.com>
+ * @author     Dennis Eichhorn <d.eichhorn@oms.com>
+ * @license    OMS License 1.0
+ * @link       http://orange-management.com
+ * @since      1.0.0
+ */
+class Uninstall extends UninstallAbstract
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function unisntall(Pool $dbPool, array $info)
+    {
+        parent::unisntall($dbPool, $info);
+    }
+}
