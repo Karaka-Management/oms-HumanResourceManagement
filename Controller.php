b44a1a6db447416328757fbc0244730f476d22ec
Dennis Eichhorn <spl1nes.com@googlemail.com>
2016-01-10 19:13:00 +0100

    Admin dependency added

--- Controller.php
+++ Controller.php
@@ -88,7 +88,9 @@ class Controller extends ModuleAbstract implements WebInterface
      * @var \string
      * @since 1.0.0
      */
-    protected static $dependencies = [];
+    protected static $dependencies = [
+        'Admin',
+    ];
 
     /**
      * Routing elements.
