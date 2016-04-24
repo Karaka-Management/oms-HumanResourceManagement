9c219ca7c523ea9d7c46885f7d2884d411b0e688
Dennis Eichhorn <spl1nes.com@googlemail.com>
2016-04-24 20:47:29 +0200

    Unit test bug fixes

--- Models/EmployeeMapper.php
+++ Models/EmployeeMapper.php
@@ -16,8 +16,6 @@
 namespace Modules\HumanResourceManagement\Models;
 
 use Modules\Admin\Models\AccountMapper;
-use Modules\HumanResourceManagement\Models\EmployeeHistoryMapper;
-use Modules\HumanResourceManagement\Models\EmployeeStatus;
 use phpOMS\DataStorage\Database\DataMapperAbstract;
 
 class EmployeeMapper extends DataMapperAbstract
@@ -31,7 +29,7 @@ class EmployeeMapper extends DataMapperAbstract
      */
     protected static $columns = [
         'hr_staff_id'         => ['name' => 'hr_staff_id', 'type' => 'int', 'internal' => 'id'],
-        'hr_staff'     => ['name' => 'hr_staff', 'type' => 'int', 'internal' => 'account'],
+        'hr_staff_account'     => ['name' => 'hr_staff_account', 'type' => 'int', 'internal' => 'account'],
     ];
 
     protected static $ownsOne = [
@@ -42,20 +40,6 @@ class EmployeeMapper extends DataMapperAbstract
     ];
 
     protected static $hasMany = [
-        'history' => [
-            'mapper' => EmployeeHistoryMapper::class,
-            'relationmapper' => EmployeeHistoryMapper::class,
-            'table' => 'hr_history',
-            'src' => 'hr_history_staff',
-            'dst' => null,
-        ],
-        'status' => [
-            'mapper' => EmployeeStatus::class,
-            'relationmapper' => EmployeeStatus::class,
-            'table' => 'hr_status',
-            'src' => 'hr_status_staff',
-            'dst' => null,
-        ],
     ];
 
     /**
