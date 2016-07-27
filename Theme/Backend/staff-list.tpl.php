5bbcad85b1d676d6c782e0175c05ae6936c2a2b6
Dennis Eichhorn <spl1nes.com@googlemail.com>
2016-07-27 13:17:50 +0200

    Translation fixes (0 still missing)

--- Theme/Backend/staff-list.tpl.php
+++ Theme/Backend/staff-list.tpl.php
@@ -30,14 +30,14 @@ echo $this->getData('nav')->render(); ?>
 
 <div class="box w-100">
     <table class="table">
-        <caption><?= $this->l11n->getText('HumanResourceManagement', 'Backend', 'Staff'); ?></caption>
+        <caption><?= $this->getText('Staff'); ?></caption>
         <thead>
         <tr>
-            <td><?= $this->l11n->getText(0, 'Backend', 'ID'); ?>
-            <td class="wf-100"><?= $this->l11n->getText('HumanResourceManagement', 'Backend', 'Name'); ?>
-            <td><?= $this->l11n->getText('HumanResourceManagement', 'Backend', 'Position'); ?>
-            <td><?= $this->l11n->getText('HumanResourceManagement', 'Backend', 'Department'); ?>
-            <td><?= $this->l11n->getText('HumanResourceManagement', 'Backend', 'Status'); ?>
+            <td><?= $this->getText('ID'); ?>
+            <td class="wf-100"><?= $this->getText('Name'); ?>
+            <td><?= $this->getText('Position'); ?>
+            <td><?= $this->getText('Department'); ?>
+            <td><?= $this->getText('Status'); ?>
         <tfoot>
         <tr><td colspan="5"><?= $footerView->render(); ?>
         <tbody>
@@ -50,7 +50,7 @@ echo $this->getData('nav')->render(); ?>
                 <td><a href="<?= $url; ?>"><?= $value->getNewestStatus()->getStatus(); ?></a>
         <?php endforeach; ?>
         <?php if($c === 0) : ?>
-            <tr><td colspan="5" class="empty"><?= $this->l11n->getText(0, 'Backend', 'Empty'); ?>
+            <tr><td colspan="5" class="empty"><?= $this->getText('Empty'); ?>
         <?php endif; ?>
     </table>
 </div>
