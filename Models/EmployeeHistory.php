a9114d6170967a3ef048f3a6e914dcec02698ad4
Dennis Eichhorn <spl1nes.com@googlemail.com>
2017-07-26 17:48:08 +0200

    Fix file author and copyright

--- Models/EmployeeHistory.php
+++ Models/EmployeeHistory.php
@@ -6,7 +6,6 @@
  *
  * @category   TBD
  * @package    TBD
- * @author     OMS Development Team <dev@oms.com>
  * @copyright  Dennis Eichhorn
  * @license    OMS License 1.0
  * @version    1.0.0
@@ -20,7 +19,6 @@ namespace Modules\HumanResourceManagement\Models;
  *
  * @category   HumanResources
  * @package    Framework
- * @author     OMS Development Team <dev@oms.com>
  * @license    OMS License 1.0
  * @link       http://orange-management.com
  * @since      1.0.0
