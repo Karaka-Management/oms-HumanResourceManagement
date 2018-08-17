d96e91ebf7e1bc2f3c9db87619c2cbbfac4ed760
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-08-17 20:02:29 +0200

    fix #96 fix #97

--- Admin/Routes/Web/Backend.php
+++ Admin/Routes/Web/Backend.php
@@ -1,30 +1,53 @@
 <?php
 
 use phpOMS\Router\RouteVerb;
+use phpOMS\Account\PermissionType;
+use Modules\HumanResourceManagement\Models\PermissionState;
+use Modules\HumanResourceManagement\Controller;
 
 return [
     '^.*/backend/hr/staff/list.*$' => [
         [
             'dest' => '\Modules\HumanResourceManagement\Controller:viewHrStaffList',
             'verb' => RouteVerb::GET,
+            'permission' => [
+                'module' => Controller::MODULE_NAME,
+                'type'  => PermissionType::READ,
+                'state' => PermissionState::HR,
+            ],
         ],
     ],
     '^.*/backend/hr/staff/profile.*$' => [
         [
             'dest' => '\Modules\HumanResourceManagement\Controller:viewHrStaffProfile',
             'verb' => RouteVerb::GET,
+            'permission' => [
+                'module' => Controller::MODULE_NAME,
+                'type'  => PermissionType::READ,
+                'state' => PermissionState::HR,
+            ],
         ],
     ],
     '^.*/backend/hr/staff/create.*$' => [
         [
             'dest' => '\Modules\HumanResourceManagement\Controller:viewHrStaffCreate',
             'verb' => RouteVerb::GET,
+            'permission' => [
+                'module' => Controller::MODULE_NAME,
+                'type'  => PermissionType::CREATE,
+                'state' => PermissionState::HR,
+            ],
         ],
     ],
     '^.*/backend/hr/department/list.*$' => [
         [
             'dest' => '\Modules\HumanResourceManagement\Controller:viewHrDepartmentList',
             'verb' => RouteVerb::GET,
+            'permission' => [
+                'module' => Controller::MODULE_NAME,
+                'type'  => PermissionType::READ,
+                'state' => PermissionState::DEPARTMENT,
+            ],
         ],
     ],
 ];
---
+++ Models/PermissionState.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.2
+ *
+ * @package    Modules\HumanResourceManagement
+ * @copyright  Dennis Eichhorn
+ * @license    OMS License 1.0
+ * @version    1.0.0
+ * @link       http://website.orange-management.de
+ */
+declare(strict_types=1);
+
+namespace Modules\HumanResourceManagement\Models;
+
+use phpOMS\Stdlib\Base\Enum;
+
+/**
+ * Permision state enum.
+ *
+ * @package    Modules\HumanResourceManagement
+ * @license    OMS License 1.0
+ * @link       http://website.orange-management.de
+ * @since      1.0.0
+ */
+abstract class PermissionState extends Enum
+{
+    public const HR         = 1;
+    public const DEPARTMENT = 2;
+}
