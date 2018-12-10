245f69176848f8b00b07fbaf8d98fd3c56b22f39
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-12-10 18:13:38 +0100

    Fix null typehint order

--- Models/EmployeeMapper.php
+++ Models/EmployeeMapper.php
@@ -26,7 +26,7 @@ final class EmployeeMapper extends DataMapperAbstract
     /**
      * Columns.
      *
-     * @var array<string, array<string, string|bool>>
+     * @var array<string, array<string, bool|string>>
      * @since 1.0.0
      */
     protected static $columns = [
