6d2a99063a7f6121e9b435d9015ccb9b75a59b0f
Dennis Eichhorn <spl1nes.com@googlemail.com>
2019-05-14 22:02:49 +0200

    Start changing styles

--- Theme/Backend/staff-list.tpl.php
+++ Theme/Backend/staff-list.tpl.php
@@ -28,7 +28,7 @@ echo $this->getData('nav')->render(); ?>
 <div class="row">
     <div class="col-xs-12">
         <div class="box wf-100">
-            <table class="table darkred">
+            <table class="default">
                 <caption><?= $this->getHtml('Staff') ?><i class="fa fa-download floatRight download btn"></i></caption>
                 <thead>
                 <tr>
