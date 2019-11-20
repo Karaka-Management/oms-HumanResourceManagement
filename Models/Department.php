aea56bfc612397882ce20805bf40fee9ee0ab847
Dennis Eichhorn <spl1nes.com@googlemail.com>
2019-11-20 22:28:04 +0100

    cleanup and tests added for ci/cd

---
+++ Models/Department.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.4
+ *
+ * @package   Modules\HumanResourceManagement\Models
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 1.0
+ * @version   1.0.0
+ * @link      https://orange-management.org
+ */
+declare(strict_types=1);
+
+namespace Modules\HumanResourceManagement\Models;
+
+/**
+ * Department class.
+ *
+ * @package Modules\HumanResourceManagement\Models
+ * @license OMS License 1.0
+ * @link    https://orange-management.org
+ * @since   1.0.0
+ */
+class Department
+{
+}
