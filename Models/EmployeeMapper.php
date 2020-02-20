38f4345d3ba7a1a41805c811fc1c29120cbc3573
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-02-20 18:58:38 +0100

    fixes #209

--- Models/EmployeeMapper.php
+++ Models/EmployeeMapper.php
@@ -32,7 +32,7 @@ final class EmployeeMapper extends DataMapperAbstract
     /**
      * Columns.
      *
-     * @var array<string, array{name:string, type:string, internal:string, autocomplete?:bool, annotations?:array}>
+     * @var array<string, array{name:string, type:string, internal:string, autocomplete?:bool, readonly?:bool, annotations?:array}>
      * @since 1.0.0
      */
     protected static array $columns = [
