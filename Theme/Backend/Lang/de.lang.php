486407e774ef63c34872ab3b590d0dd22faf7c37
Dennis Eichhorn <spl1nes.com@googlemail.com>
2021-02-08 19:10:15 +0100

    test language/export/import which changed a lot of lang files

---
+++ Theme/Backend/Lang/de.lang.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 8.0
+ *
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 1.0
+ * @version   1.0.0
+ * @link      https://orange-management.org
+ */
+declare(strict_types=1);
+
+return ['HumanResourceManagement' => [
+    'Account' => '',
+    'Active' => '',
+    'Birthday' => '',
+    'Clocking' => '',
+    'Contracts' => '',
+    'CreateFromAccount' => '',
+    'Department' => '',
+    'Departments' => '',
+    'Documents' => '',
+    'Education' => '',
+    'Email' => '',
+    'Employee' => '',
+    'Employees' => '',
+    'End' => '',
+    'Evaluations' => '',
+    'General' => '',
+    'History' => '',
+    'Inactive' => '',
+    'Name' => '',
+    'Parent' => '',
+    'Personnel' => '',
+    'Position' => '',
+    'Remarks' => '',
+    'Shifts' => '',
+    'Staff' => '',
+    'Start' => '',
+    'Status' => '',
+    'Unit' => '',
+    'Vacation' => '',
+    'Work' => '',
+    'ProfileImage' => '',
+    'Phone' => '',
+]];
