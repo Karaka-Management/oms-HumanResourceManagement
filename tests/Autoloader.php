b3215b7c3c088901af033530471d34be3e5b9e6f
Dennis Eichhorn <spl1nes.com@googlemail.com>
2021-02-09 18:22:20 +0100

    draft individual module test environments

---
+++ tests/Autoloader.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 8.0
+ *
+ * @package   Modules/tests
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 1.0
+ * @version   1.0.0
+ * @link      https://orange-management.org
+ */
+declare(strict_types=1);
+
+namespace tests;
+
+\spl_autoload_register('\tests\Autoloader::defaultAutoloader');
+
+/**
+ * Autoloader class.
+ *
+ * @package tests
+ * @license OMS License 1.0
+ * @link    https://orange-management.org
+ * @since   1.0.0
+ */
+final class Autoloader
+{
+    /**
+     * Base paths for autoloading
+     *
+     * @var string[]
+     * @since 1.0.0
+     */
+    private static $paths = [
+        __DIR__ . '/../',
+        __DIR__ . '/../Orange-Management/',
+        __DIR__ . '/../../',
+    ];
+
+    /**
+     * Constructor.
+     *
+     * @since 1.0.0
+     * @codeCoverageIgnore
+     */
+    private function __construct()
+    {
+    }
+
+    /**
+     * Add base path for autoloading
+     *
+     * @param string $path Absolute base path with / at the end
+     *
+     * @return void
+     *
+     * @since 1.0.0
+     */
+    public static function addPath(string $path) : void
+    {
+        self::$paths[] = $path;
+    }
+
+    /**
+     * Loading classes by namespace + class name.
+     *
+     * @param string $class Class path
+     *
+     * @example Autoloader::defaultAutoloader('\phpOMS\Autoloader') // void
+     *
+     * @return void
+     *
+     * @throws AutoloadException Throws this exception if the class to autoload doesn't exist. This could also be related to a wrong namespace/file path correlation.
+     *
+     * @since 1.0.0
+     */
+    public static function defaultAutoloader(string $class) : void
+    {
+        $class = \ltrim($class, '\\');
+        $class = \str_replace(['_', '\\'], '/', $class);
+
+        foreach (self::$paths as $path) {
+            $file = $path . $class . '.php';
+            $file = \str_replace('/Modules/', '/', $file);
+
+            if (\is_file($file)) {
+                include_once $file;
+
+                return;
+            }
+        }
+    }
+}
