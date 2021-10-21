99b80281038980c6d730c8f9b638418959c6ba23
Dennis Eichhorn <spl1nes.com@googlemail.com>
2021-10-21 22:16:06 +0200

    more tests and make classes final

---
+++ tests/Models/NullEmployeeHistory.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 8.0
+ *
+ * @package   tests
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 1.0
+ * @version   1.0.0
+ * @link      https://orange-management.org
+ */
+declare(strict_types=1);
+
+namespace Modules\HumanResourceManagement\tests\Models;
+
+use Modules\HumanResourceManagement\Models\NullEmployeeHistory;
+
+/**
+ * @internal
+ */
+final class Null extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @covers Modules\HumanResourceManagement\Models\NullEmployeeHistory
+     * @group framework
+     */
+    public function testNull() : void
+    {
+        self::assertInstanceOf('\Modules\HumanResourceManagement\Models\EmployeeHistory', new NullEmployeeHistory());
+    }
+
+    /**
+     * @covers Modules\HumanResourceManagement\Models\NullEmployeeHistory
+     * @group framework
+     */
+    public function testId() : void
+    {
+        $null = new NullEmployeeHistory(2);
+        self::assertEquals(2, $null->getId());
+    }
+}
