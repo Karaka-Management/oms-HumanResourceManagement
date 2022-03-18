9534cfee23d0399f4aeca3d5e0398b0196f89c1d
Dennis Eichhorn <spl1nes.com@googlemail.com>
2022-03-19 00:02:43 +0100

    bug fixes from the day before

--- Admin/Install/Media.php
+++ Admin/Install/Media.php
@@ -29,14 +29,14 @@ class Media
     /**
      * Install media providing
      *
-     * @param string              $path Module path
      * @param ApplicationAbstract $app  Application
+     * @param string              $path Module path
      *
      * @return void
      *
      * @since 1.0.0
      */
-    public static function install(string $path, ApplicationAbstract $app) : void
+    public static function install(ApplicationAbstract $app, string $path) : void
     {
         \Modules\Media\Admin\Installer::installExternal($app, ['path' => __DIR__ . '/Media.install.json']);
     }
