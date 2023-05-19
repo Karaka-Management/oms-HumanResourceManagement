a7e444c9ca5410e263eec4d9dbd77150faa39ed3
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-05-19 02:37:35 +0000

    test fixes + new test data

--- Admin/Updater.php
+++ Admin/Updater.php
@@ -26,4 +26,11 @@ use phpOMS\Module\UpdaterAbstract;
  */
 final class Updater extends UpdaterAbstract
 {
+    /**
+     * Path of the file
+     *
+     * @var string
+     * @since 1.0.0
+     */
+    public const PATH = __DIR__;
 }
