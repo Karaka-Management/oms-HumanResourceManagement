5afd8bfdd781618ee2e94324e3eefd51d66e1358
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-12-08 21:52:33 +0000

    bug fixes

--- Models/EmployeeEducationHistory.php
+++ Models/EmployeeEducationHistory.php
@@ -14,9 +14,9 @@ declare(strict_types=1);
 
 namespace Modules\HumanResourceManagement\Models;
 
-use Modules\Admin\Models\Address;
 use Modules\Media\Models\Media;
 use Modules\Media\Models\NullMedia;
+use phpOMS\Stdlib\Base\Address;
 
 /**
  * Employee class.
